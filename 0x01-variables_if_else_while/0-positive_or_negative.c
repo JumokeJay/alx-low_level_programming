c75723ae1746fe3ae748d379073f862b6c286652
JumokeJay <Jayrevam@gmail.com>
2023-07-17 15:05:56 -0700

    nagato

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,27 +4,26 @@
 /* more headers goes there */
 
 /*
- * main - this is the entry point
+ * main - Entry point
  * Return: should always be 0 (success)
- * betty style doc for function main goes there 
+ * betty style doc for function main goes there
  */
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-	printf("The number is positive\n");
+	printf("is positive\n");
 	}
 	else if (n < 0)
 	{
-	printf("The number is negative\n");
+	printf("is negative\n");
 	}
-	else 
+	else
 	{
-	printf("The number is zero\n");
+	printf("is zero\n");
 	}
 	return (0);
 }
