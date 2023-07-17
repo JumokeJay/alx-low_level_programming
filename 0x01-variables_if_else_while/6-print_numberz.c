30b6df2045a557fc6cd7274a2b2844f6d915e5e4
JumokeJay <Jayrevam@gmail.com>
2023-07-17 16:53:26 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * main - Entry point as usual 
+ * Return: should always be 0 (success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+	putchar('0' + num);
+	num++;
+	}
+
+	putchar('\n');
+	return (0);
+}
