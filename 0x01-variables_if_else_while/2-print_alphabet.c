b922f83925e7371e4d45379694fca39ba02c049f
JumokeJay <Jayrevam@gmail.com>
2023-07-17 15:58:55 -0700

    printdamit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,15 @@
  * main - Entry point for alphabets
  * Return: should always be 0 (success)
  */
-int main(void)
+int main(void)/* main is entry point to compute this comman for alphabets */
 {
 	char alphabet;
-	while (alphabet <= 'z')
+
+	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
 	{
-	putchar(alphabet);
-	alphabet++;
+
+		putchar(alphabet);
+
 	}
 	putchar('\n');
 	return (0);
