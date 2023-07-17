19862ebea84de3e54b15ffb10ea71c40c3ebb51a
JumokeJay <Jayrevam@gmail.com>
2023-07-17 15:09:56 -0700

    positio

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,28 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
 /*
  * main - Entry point
  * Return: should always be 0 (success)
- * betty style doc for function main goes there
  */
 int main(void)
 {
 	int n;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-	printf("is positive\n");
+	printf("98 is positive\n");
 	}
 	else if (n < 0)
 	{
-	printf("is negative\n");
+	printf("-98 is negative\n");
 	}
 	else
 	{
-	printf("is zero\n");
+	printf("0 is zero\n");
 	}
 	return (0);
 }
