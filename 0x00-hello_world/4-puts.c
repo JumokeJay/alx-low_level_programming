f88d5fa7df381f547ce8f7358c02d1623b679afb
JumokeJay <Jayrevam@gmail.com>
2023-07-17 13:39:49 -0700

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - This is the entry point or stage
+ *Return: this is always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
