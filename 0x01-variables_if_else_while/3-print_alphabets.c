c1455840d63620b0cf71e823fd013019c4e5f2ab
JumokeJay <Jayrevam@gmail.com>
2023-07-17 16:08:02 -0700

    3dprint

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+/* main - Entry point that covers the entire code
+ * Return: always 0 (success)
+ * dont know whats wrong with the description of my main
+ */
+int main()
+{
+	char lowercase = 'a';
+	while (lowercase <= 'z')
+	{
+	putchar(lowercase);
+	lowercase++;
+	}
 
+	char uppercase = 'A';
+	while (uppercase <= 'Z')
+	{
+	putchar(uppercase);
+	uppercase++;
+	}
+
+	putchar('\n');
+	return 0;
+}
