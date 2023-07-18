b0829087fdf2e52852302b6fcf6148194d5dfbc0
JumokeJay <Jayrevam@gmail.com>
2023-07-18 12:59:43 -0700

    char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point
+ * Return: always 0 (success)
+ * failed to use two asteric yesterday hope its works today
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is for a nested loop which is basically a loop inside a loop
