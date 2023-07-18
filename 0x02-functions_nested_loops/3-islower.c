ef19e3ce6970b4d3ab3d472a7a2af049aff0d030
JumokeJay <Jayrevam@gmail.com>
2023-07-18 15:24:22 -0700

    lowaa

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,10 +4,8 @@
  * main - this is the entry point
  * Return - this is always 0 (success)
  * _islower - this is used to check the case of the alphabet
- * @parameterc - this is used to control the alphabet
+ * @c - character to be checked
  */
-
-
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
