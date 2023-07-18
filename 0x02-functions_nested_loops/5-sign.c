a0f0e82cf75eed863e5d4329fc1db18f0a53f4d5
JumokeJay <Jayrevam@gmail.com>
2023-07-18 16:01:23 -0700

    finally

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,27 +2,27 @@
 #include <stdio.h>
 
 /**
- * print_sign - to show if negative or positive
- * @n - the parameter for the determinant
+ * print_sign - to show if n is negative or positive
+ * @n - the parameter for the determined
  *
- * return - will show 1 if true and 0 if false
+ * Return - 1 if positive, 0 if 0, and -1 if negative
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar(43);
 		return (1);
-		putchar('+');
 	}
 	else if (n == 0)
 	{
+		_putchar(48);
 		return (0);
-		putchar('0');
 	}
 	else if (n < 0)
-	{
+	{	
+		_putchar(45);
 		return (-1);
-		putchar('-');
 	}
 
 	return (2);
