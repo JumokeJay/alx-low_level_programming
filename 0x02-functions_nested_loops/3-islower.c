122eadf278ee0c83f1ce733951e8c467dec5ea0f
JumokeJay <Jayrevam@gmail.com>
2023-07-18 15:27:22 -0700

    lowercasewahala

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,6 +6,11 @@
  * _islower - this is used to check the case of the alphabet
  * @c - character to be checked
  */
+/**
+ * _islower - thi is used to check the case of the alphabet
+ * @c - check the character
+ * return - 1 if it is lowercase and 0 if it not
+ */
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
