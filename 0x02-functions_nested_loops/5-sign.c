a411bb66d90372423dd4ea8b07fba4b8804d8000
JumokeJay <Jayrevam@gmail.com>
2023-07-18 16:03:28 -0700

    final

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,9 +3,9 @@
 
 /**
  * print_sign - to show if n is negative or positive
- * @n - the parameter for the determined
+ * @n: the parameter for the determined
  *
- * Return - 1 if positive, 0 if 0, and -1 if negative
+ * Return: 1 if positive, 0 if 0, and -1 if negative
  */
 int print_sign(int n)
 {
@@ -20,7 +20,7 @@ int print_sign(int n)
 		return (0);
 	}
 	else if (n < 0)
-	{	
+	{
 		_putchar(45);
 		return (-1);
 	}
