36ff0e4b0760ebcbfd692ff05ca0c6e778ed7db0
JumokeJay <Jayrevam@gmail.com>
2023-07-17 17:16:10 -0700

    bas16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- * main - this is the Entry point 
+ * main - this is the Entry point
  * Return: always should be 0 (success)
  */
 
@@ -8,17 +8,20 @@ int main(void)
 {
 	char hex_digit = '0';
 
-	while (hex_digit <= '9') {
+	while (hex_digit <= '9')
+	{
 	putchar(hex_digit);
 	hex_digit++;
 }
 
 	hex_digit = 'a';
-	while (hex_digit <= 'f') {
+	while (hex_digit <= 'f')
+	{
 	putchar(hex_digit);
 	hex_digit++;
 	}
 	putchar('\n');
 
 	return (0);
+
 }
