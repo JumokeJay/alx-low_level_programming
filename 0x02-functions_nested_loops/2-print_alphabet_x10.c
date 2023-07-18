e0aacad21d065dd2d8915a0a40d608ad9918e855
JumokeJay <Jayrevam@gmail.com>
2023-07-18 14:59:06 -0700

    x10alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - this is the entry point
+ * print_alphabet_x10 - this is the function for the printing os alphabet 10 times
+ * Return - always should be 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+char alphabet = 'a';
+int repeat = 10;
+
+while (repeat > 0)
+{
+char currentalphabet = alphabet;
+
+while (currentalphabet <= 'z')
+{
+putchar(currentalphabet);
+currentalphabet++;
+}
+
+repeat--;
+
+if (repeat > 0)
+putchar('\n');
+}
+putchar('\n');
+}
