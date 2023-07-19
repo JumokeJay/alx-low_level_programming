0cae809e5de47ac0a1603718c72127a9156b42a6
JumokeJay <Jayrevam@gmail.com>
2023-07-18 17:23:18 -0700

    check4

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - this is the entry point
- * Return - this is always 0 (success)
  * _islower - this is used to check the case of the alphabet
- * @c - character to be checked
- */
-/**
- * _islower - thi is used to check the case of the alphabet
  * @c - check the character
  * return - 1 if it is lowercase and 0 if it not
  */
@@ -18,4 +12,3 @@ if (c >= 'a' && c <= 'z')
 else
 	return (0);
 }
-
