8ee18ddef83a6e003cb5d5835e65920919d32a5e
JumokeJay <Jayrevam@gmail.com>
2023-07-18 17:12:31 -0700

    10add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * add - sums up the two integers 
+ * add - sums up the two integers
  * @i: the first int
  * @j: the second int
  * Return: to produce the sum
  */
-int add(int i,int j)
+int add(int i, int j)
 {
 int result;
 result = i + j;
