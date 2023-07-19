dc7652c771b865defb6f519bbac0161094e62724
JumokeJay <Jayrevam@gmail.com>
2023-07-18 17:24:51 -0700

    check5

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,8 @@
 #include "main.h"
 /**
  * _islower - this is used to check the case of the alphabet
- * @c - check the character
- * return - 1 if it is lowercase and 0 if it not
+ * @c: check the character
+ * return: 1 if it is lowercase and 0 if it not
  */
 int _islower(int c)
 {
