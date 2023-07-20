8bc07a60aef110a7951fccecf206f6081811574d
JumokeJay <Jayrevam@gmail.com>
2023-07-20 14:05:27 -0700

    check4a

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - to call up numbers from 0 to 9
+ *
+ * Return: to print out the numbers
+ */
+void print_numbers(void)
+{
+        int i = 0;
+while (i < 10)
+{
+	_putchar(i + '0');
+	i++;
+}
+	_putchar('\n');
+}
