98db0567b62853f9470f9fd0432301592bea89c8
JumokeJay <Jayrevam@gmail.com>
2023-07-20 14:55:46 -0700

    check6a

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - the function to demand more numbers
+ * Return: what it is bring out
+ */
+void more_numbers(void)
+{
+	int a, c;
+	for (c = 0; c < 10; c++)
+	{
+	for (a = 0; a <= 14; a++)
+	{
+	if (a >= 10)
+	{
+	_putchar('0' + (a / 10));
+	}
+	_putchar('0' + (a % 10));
+	}
+	_putchar('\n');
+	}
+}
