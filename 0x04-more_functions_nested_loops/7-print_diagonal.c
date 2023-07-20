a09fa96d99e029ce024b5461451f9aa71044f3d6
JumokeJay <Jayrevam@gmail.com>
2023-07-20 15:23:58 -0700

    check8c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,21 +7,21 @@
  * Return: void
  */
 void print_diagonal(int n)
-{
-	int i, spaces;
-
-	if (n > 0)
 	{
-	for (i = 1; i <= n; i++)
+	if (n <= 0)
 	{
-	for (spaces = 0; spaces < i; spaces++)
+	_putchar('\n');
+	}
+	else
+	{
+	for (int i = 0; i < n; i++)
+	{
+	for (int j = 0; j < i; j++)
 	{
 	_putchar(' ');
 	}
-	_putchar(92);
+	_putchar('\\');
 	_putchar('\n');
 	}
 	}
-	else
-	_putchar('\n');
 }
