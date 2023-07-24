9e91a5d60b5312f396dc7fcb3edb9de26a991eb4
JumokeJay <Jayrevam@gmail.com>
2023-07-24 15:07:56 -0700

    check7a

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * puts2 - the function to print the character of a string
+ * @str: the character to be defined
+ * Return: the print of the string
+ */
+
+void puts2(char *str)
+{
+	int index = 0;
+
+	while (str[index] != '\0')
+	{
+	putchar(str[index]);
+	
+	index += 2;
+	
+	if (str == NULL)
+	return;
+	}
+	putchar('\n');
+}
