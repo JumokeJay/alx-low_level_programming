6bb8d7f6a10605f63beaaa3a908f263d0c6a979e
JumokeJay <Jayrevam@gmail.com>
2023-07-27 13:21:24 -0700

    check2b

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,13 +11,15 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	char *dest_end = dest;
+	
 	while (*dest_end != '\0')
 	{
 	dest_end++;
 	}
-
 	while (*src != '\0' && n > 0)
+	
 	{
+
 	*dest_end = *src;
 	dest_end++;
 	src++;
