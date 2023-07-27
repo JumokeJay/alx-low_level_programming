4d57f3431b7497ba6dfcdbaf37eab2adc04a2f6f
JumokeJay <Jayrevam@gmail.com>
2023-07-27 13:37:00 -0700

    check3a

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncpy - the function that cop0ies a string
+ * @dest: the destination of the string
+ * @sr: where it is copied from
+ * @n: the string itself
+ * Return: show the result i guess
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+	dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+	dest[i] = '\0';
+	}
+	return (dest);
+}
