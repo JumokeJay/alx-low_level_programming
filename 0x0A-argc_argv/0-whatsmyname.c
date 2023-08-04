749ae4ecde7975c002d2b6afb15edfca826e5b15
JumokeJay <Jayrevam@gmail.com>
2023-08-04 12:45:12 -0700

    check1a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - the function of the program
+ * @argc: the count of the arguments
+ * @argv: the array of the arguments
+ * Return: the result
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+	printf("%s\n", argv[0]);
+	}
+	return (0);
+}
