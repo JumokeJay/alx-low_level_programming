b8ee40fb174d3b759d2eef8f388c953f07e48245
JumokeJay <Jayrevam@gmail.com>
2023-08-07 12:39:52 -0700

    check2a

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - the function that directs the pointer of the memory
+ * @str: the string to be allocated to the memory
+ * Return: the result of the code
+ */
+
+char *_strdup(char *str)
+{
+	char *duplicate;
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+
+	duplicate = (char *)malloc(strlen(str) + 1);
+    if (duplicate == NULL)
+	{
+	return (NULL);
+	}
+
+	strcpy(duplicate, str);
+
+	return (duplicate);
+}
