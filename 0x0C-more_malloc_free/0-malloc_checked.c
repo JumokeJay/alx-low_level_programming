51a6b296cc78c55c975a6b6af19d97255fcdfde0
JumokeJay <Jayrevam@gmail.com>
2023-08-10 12:30:47 -0700

    check1a

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - the function that allocates the memory
+ * @b: the integer to be assigned
+ * Return: the result
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+	if (ptr == NULL)
+	{
+	fprintf(stderr, "Memory allocation failed\n");
+	exit(98);
+	}
+	return (ptr);
+}
