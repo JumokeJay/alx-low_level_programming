90d29ef9ed2a4f4febe8a2da3abeff3d9a6c0da5
JumokeJay <Jayrevam@gmail.com>
2023-08-16 15:12:27 -0700

    check6c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,14 +12,52 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 	return (NULL);
+
 	for (size = 0; *(str + size) != '\0'; size++);
 	ar = malloc((size + 1) * sizeof(char));
+
 	if (ar == NULL)
 	return (NULL);
+
 	for (i = 0; str[i] != '\0'; i++)
 {
 	ar[i] = str[i];
 }
 	ar[i] = '\0';
 	return (ar);
+
+}
+/**
+ * new_dog - creates a new dog struct
+ * @name: new name of the dog
+ * @age: new age
+ * @owner: new name of owner
+ * Return: pointer to new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *dog;
+	char *newname;
+	char *newowner;
+	dog = malloc(sizeof(struct dog));
+	if (dog == NULL)
+	return (NULL);
+	newname = _strdup(name);
+	dog->name = newname;
+	if (dog->name == NULL)
+{
+	free(dog);
+	return (NULL);
+}
+	newowner = _strdup(owner);
+	dog->owner = newowner;
+	if (dog->owner == NULL)
+{
+	free(dog->name);
+	free(dog);
+	return (NULL);
+}
+	dog->age = age;
+	return (dog);
 }
