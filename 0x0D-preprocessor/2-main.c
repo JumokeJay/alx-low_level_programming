917c06155546bba2286625bd4ad8e6502f39a1b3
JumokeJay <Jayrevam@gmail.com>
2023-08-16 14:12:50 -0700

    check3a

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char *p = __FILE__;
+	while (*p)
+	{
+	_putchar(*p);
+	p++;
+	}
+	_putchar(10);
+	return (0);
+}
