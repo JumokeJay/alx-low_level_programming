2c43cbcd2521dbf46c0608143c2e29ff5f546c46
JumokeJay <Jayrevam@gmail.com>
2023-08-24 13:41:28 -0700

    check 1a

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * print_list - the function to print the list
+ * @h: the parameter h to be determined
+ * Retturn: the result showing the list
+ */
+
+typedef struct list_s
+{
+	char *str;
+	struct list_s *next;
+}
+list_t;
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+{
+	if (h->str == NULL)
+	printf("[%lu] (nil)\n", count);
+	else
+	printf("[%lu] %s\n", count, h->str);
+
+	h = h->next;
+	count++;
+}
+	return (count);
+}
