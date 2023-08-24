d084ebaa791e4f8cae94f315816056bf7b8e6c19
JumokeJay <Jayrevam@gmail.com>
2023-08-24 15:32:37 -0700

    check7a

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* print_message - as the name implies
+ */
+
+void print_message(void)__attribute__((constructor));
+void print_message(void) 
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,49 +8,23 @@
  * Return: the result of the code
  */
 
-typedef struct list_s 
-{
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
-
-char *custom_strdup(const char *src)
-{
-	if (src == NULL)
-	return (NULL);
-
-	char *dest = malloc(strlen(src) + 1);
-	if (dest == NULL)
-	return (NULL);
-
-	strcpy(dest, src);
-	return (dest);
-}
 
 list_t *add_node(list_t **head, const char *str)
 {
-	if (str == NULL)
-	return (NULL);
-
+	list_t *arrival;
 	unsigned int len = 0;
+
 	while (str[len])
 	len++;
 
-	list_t *new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	arrival = malloc(sizeof(list_t));
+	if (!arrival)
 	return (NULL);
 
-	new_node->str = custom_strdup(str);
-	if (new_node->str == NULL)
-	{
-	free(new_node);
-	return (NULL);
-	}
-
-	new_node->len = len;
-	new_node->next = *head;
-	*head = new_node;
+	arrival->str = strdup(str);
+	arrival->len = len;
+	arrival->next = (*head);
+	(*head) = arrival;
 
 	return (*head);
 }
