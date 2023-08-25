62d1fbb469c56777e97a14e0272473318dd552b7
JumokeJay <Jayrevam@gmail.com>
2023-08-25 01:49:39 -0700

    check9a

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,9 @@
-section .data
-    hello db "Hello, Holberton", 0
-    format db "%s", 10, 0
-
-section .text
-    global main
-    extern printf
-
+global    main
+	extern    printf
 main:
-    sub rsp, 8                ; Align the stack (64-bit calling convention)
-    push rdi                  ; Save registers that are used
-
-    lea rdi, [format]         ; Load the address of the format string
-    lea rsi, [hello]          ; Load the address of the hello string
-    call printf              ; Call printf with the strings
-
-    pop rdi                   ; Restore saved registers
-    add rsp, 8                ; Restore the stack alignment
-
-	ret                       ; Return from the main function
+	mov   edi, format
+	xor   eax, eax
+	call  printf
+	mov 	eax, 0
+	ret
+format: db `Hello, Holberton\n`,0
