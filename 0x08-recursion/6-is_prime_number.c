34133af9b03215d0ea45ea49240ccec55cdf2c5e
JumokeJay <JumokeJay@github.com>
2023-09-28 15:05:04 -0700

    check7a

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * is_prime_helper - the function to get the prime number
+ * @n: the integer
+ * @divisor: the mathematical divisor
+ * Return: the result
+ */
+
+int is_prime_helper(int n, int divisor)
+{
+	if (divisor == 1)
+	{
+	return (1);
+	}
+
+	if (n % divisor == 0)
+	{
+	return (0);
+	}
+	return (is_prime_helper(n, divisor - 1));
+}
+
+int is_prime_number(int n)
+	{
+	if (n <= 1)
+	{
+	return (0);
+	}
+
+	return (is_prime_helper(n, n - 1));
+}
