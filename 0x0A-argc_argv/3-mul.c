c1fb2164078eff25c8a1ee0076a1c3d3a81c1aef
JumokeJay <JumokeJay@github.com>
2023-10-06 14:06:54 -0700

    check2b

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 	return (1);
 	}
 
-	mul = atoi(argv[argc 2]) * atoi(argv[argc - 1]);
+	mul = atoi(argv[argc - 2]) * atoi(argv[argc - 1]);
 
 	printf("%d\n", mul);
 
