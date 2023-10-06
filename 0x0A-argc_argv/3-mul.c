f9afbee5059c79b2ec9301064d4492126e3e8217
JumokeJay <JumokeJay@github.com>
2023-10-06 14:25:52 -0700

    check7a

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,13 +12,13 @@ int main(int argc, char *argv[])
 {
 	int mul;
 
-	if (argc < 3)
+	if (argc != 3)
 	{
 	printf("Error\n");
 	return (1);
 	}
 
-	mul = atoi(argv[argc - 2]) * atoi(argv[argc - 1]);
+	mul = atoi(argv[1]) * atoi(argv[2]);
 
 	printf("%d\n", mul);
 
