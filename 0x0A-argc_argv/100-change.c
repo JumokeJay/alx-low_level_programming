ffd232ba058651a1864cdaf40839dbd145010529
JumokeJay <JumokeJay@github.com>
2023-10-06 14:21:59 -0700

    check6a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,13 +10,17 @@
 
 int main(int argc, char *argv[])
 {
+	int cents, count, i;
+	int coins[] = {25, 10, 5, 2, 1};
+	int numCoins = sizeof(coins) / sizeof(coins[0]);
+
 	if (argc != 2)
 	{
 	printf("Error\n");
 	return (1);
 	}
 
-	int cents = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
 	if (cents < 0)
 	{
@@ -24,11 +28,9 @@ int main(int argc, char *argv[])
 	return (0);
 	}
 
-	int coins[] = {25, 10, 5, 2, 1};
-	int numCoins = sizeof(coins) / sizeof(coins[0]);
-	int count = 0;
+	count = 0;
 
-	for (int i = 0; i < numCoins; i++)
+	for (i = 0; i < numCoins; i++)
 	{
 	while (cents >= coins[i])
 	{
