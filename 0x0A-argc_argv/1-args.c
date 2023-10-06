64252e8e6d231ce0030c509ad9170d937ab2807b
JumokeJay <JumokeJay@github.com>
2023-10-06 14:01:26 -0700

    cehck1b

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - the function og the program
+ * @argc: the count of the argument
+ * @argv: the array of the arguments
+ * Return: the result of the code
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
